685e9aec1f4b20dd02ddbfd5ba59adf7b59c7571
JLooping <u16014520@tuks.co.za>
2020-11-04 23:16:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ includes for generating tracks
 --------------------------------
 */
 
+#include "TrackPartFactory.h"
 #include "LeftCurveFactory.h"
 #include "RightCurveFactory.h"
 #include "StraightFactory.h"
 
 
-/*  
+/*
 includes for generating teams???
 -----------------------------
 #include "Team.h"
@@ -24,12 +25,25 @@ int main(){
     //Create Tracks
     TrackPartFactory* trackGenerator[3];
     trackGenerator[0] = new LeftCurveFactory();
-    //trackGenerator[1] = new StraightFactory();
-    //trackGenerator[2] = new RightCurveFactory();
-    //Create Teams - which should construct() cars and create all the humans 
+    trackGenerator[1] = new StraightFactory();
+    trackGenerator[2] = new RightCurveFactory();
 
-    //Formula1* season1 = new Formula1();
-    //season1->addTracks(Tracks Vector);
+    TrackPart* trackParts[3];
+    trackParts[0] = trackGenerator[0]->createPart(10,10);
+    trackParts[1] = trackGenerator[1]->createPart(10,10);
+    trackParts[2] = trackGenerator[2]->createPart(10,10);
+
+    Track* track1 = new Track(true);
+    track1->addPart(trackParts[0]);
+    track1->addPart(trackParts[1]);
+    track1->addPart(trackParts[2]);
+
+    //Create Teams - which should construct() cars and create all the humans
+
+    Formula1* season1 = new Formula1();
+//    vector<Track*> seasonTracks;
+//    seasonTracks.push_back(track1);
+//    season1->addTracks(seasonTracks);
     //season1->addTeams(Teams Vector)
     //season1->prepareLogistics();
     //season1->startSeason(); // which should basically do the rest of the program, by running the races and generating output?
