be016f5a96d2df2a7efcdfc87ad0981faab2830d
TheySeeMeRolan <73602692+TheySeeMeRolan@users.noreply.github.com>
2020-11-04 04:22:22 +0200

    TeamResources created

---
+++ TeamResouces.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ro on 2020/11/04.
+//
+
+#include "TeamResouces.h"
---
+++ TeamResouces.h
@@ -0,0 +1,14 @@
+//
+// Created by ro on 2020/11/04.
+//
+
+#ifndef BUTTERFINGERS_TEAMRESOUCES_H
+#define BUTTERFINGERS_TEAMRESOUCES_H
+
+
+class TeamResouces {
+
+};
+
+
+#endif //BUTTERFINGERS_TEAMRESOUCES_H
