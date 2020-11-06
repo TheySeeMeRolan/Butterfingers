b0dda23665b4df60b5042bfbc789b1a8f076d125
Luke Partridge <u19010240@tuks.co.za>
2020-11-06 22:06:35 +0200

    Completed documentation of TrackHierarchy
    Also removed srand functions in cpps where it still existed

--- LeftCurveFactory.h
+++ LeftCurveFactory.h
@@ -1,3 +1,13 @@
+/**
+ * @class LeftCurveFactory
+ * @brief Concrete factory participant 
+ * 
+ * The concrete participant ConcreteFactory of Factory design pattern. 
+ * Implements the virtual functions defined in the base class.
+ * 
+ * @author Luke Partridge u19010240
+ * 
+ */
 #ifndef PROJECT_LEFTCURVEFACTORY_H
 #define PROJECT_LEFTCURVEFACTORY_H
 
@@ -6,8 +16,22 @@
 
 class LeftCurveFactory: public TrackPartFactory {
 public:
+    /**
+     * @brief create track parts (parameterized)
+     * Implementation of creating the LeftCurve trackParts with parameterized values.
+     * @param length int of part length
+     * @param difficulty int of part difficulty
+     */
     TrackPart* createPart(int l, int d);
+    /**
+     * @brief create track parts
+     * The implementation to create LeftCurve trackParts with random values for length and difficulty.
+     */
     TrackPart* createPart();
+    /**
+     * @brief destructor
+     * The concrete destructor that ensure this derived class is destroyed before the base class TrackPartFactory
+    */
     ~LeftCurveFactory();
 };
 
