a7d4dbac4d0e7ff3ec9e33647e69f5af244ce50b
TheySeeMeRolan <73602692+TheySeeMeRolan@users.noreply.github.com>
2020-11-07 16:20:23 +0200

    Strat fixes

--- QualifyingStrategy.h
+++ QualifyingStrategy.h
@@ -7,7 +7,6 @@ class QualifyingStrategy: public Strategy {
 public:
     virtual vector<Team*> race(vector<Team*> teams, Track* track, vector<vector<int>>);
     virtual int determineTeamRaceScore(Team*, int, int);
-    virtual vector<Team*> sort(vector<Team*>,vector<int>, vector<int>&);
     virtual ~QualifyingStrategy();
 };
 
