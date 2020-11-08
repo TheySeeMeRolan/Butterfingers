6d8939f120dc2d7611b8f3e048e4e193e5df88e7
JLooping <u16014520@tuks.co.za>
2020-11-08 16:05:27 +0200

    Added doxygen
    Added doxygen comments to:
    ChangeTyreCommand.h
    Race.h
    Strategy.h
    PracticeStrategy.h
    QualifyingStrategy.h
    RaceStrategy.h

--- PracticeStrategy.h
+++ PracticeStrategy.h
@@ -1,3 +1,14 @@
+/**
+ * @class PracticeStrategy
+ * @brief Concrete class.
+ *
+ * Concrete class serves as one of the ConcreteStrategy design pattern.
+ * Implements the virtual functions for PracticeStrategy.
+ *
+ * @author Janlu Pretorius u16014520
+ *
+ */
+
 #ifndef PROJECT_PRACTICESTRATEGY_H
 #define PROJECT_PRACTICESTRATEGY_H
 
@@ -5,8 +16,25 @@
 
 class PracticeStrategy: public Strategy {
 public:
+    /**
+     * @brief executes the various race stratgise.
+     * Pure virtual algorithm() of the Strategy design pattern.
+     * @param team vector of all the teams participating in the race.
+     * @param track pointer to the Track the race is being run on.
+     * @return vector of teams in the potion they placed.
+     */
     virtual vector<Team*> race(vector<Team*> teams, Track* track, vector<vector<int>>);
+
+    /**
+    * @brief Helper function to determine each teams score per kilometer of race run.
+    * @return integer of the score a team achieved in a kilometer.
+    */
     virtual int determineTeamRaceScore(Team*, int, int);
+
+    /**
+     * @brief virtual destructor
+     * Ensures that the base class Strategy is destroyed after the derived classes.
+     */
     virtual ~PracticeStrategy();
 };
 
